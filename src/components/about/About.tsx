c9c2fa164c471e168f5224dbeb9b87c64c72580a
hamzaazkar <hamzaazkar@gmail.com>
2023-11-17 15:14:29 +0500

    Complete about Section

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -60,6 +60,41 @@ export const About = () => {
 				</Stack>
 
 			</Stack>
+
+			<Stack
+				direction={{ xs: 'column', sm: 'row' }}
+				columnGap='40px'
+				marginTop='40px'
+				marginBottom='40px'
+				alignItems='center'
+			>
+				<Typography
+					variant='h5'
+					fontWeight='500'
+					lineHeight={2}
+				>
+					Hey ! i’m Hamza , 26 years old from Pakistan .An accomplished developer interested in web design and combining the art of design with the art of programming !
+				</Typography>
+
+				<Typography
+					variant='h5'
+					fontWeight='500'
+					lineHeight={2}
+				>
+					familiar with designing and implementing user friendly web applications using Html Css React and Next.js .Ability to create a full web pages design using Figma then transform it into real website
+				</Typography>
+			</Stack>
+
+			<Typography
+				variant='h3'
+				fontWeight='700'
+				noWrap
+				fontStyle='italic'
+				marginTop={2}
+			>
+				Degree :{' '}
+				<span style={{ textDecoration: 'underline', color: '#abf129' }}>BSCS</span>
+			</Typography>
 		</Container>
 	);
 };
